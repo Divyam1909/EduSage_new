eb712b8ab689618eb3aad40c7c890f604efb70d2
Divyam1909 <divyamnavin@gmail.com>
2025-04-28 21:53:23 +0530

    dep17

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import { ToastProvider } from "./components/ToastContainer.tsx";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 // Create the root without StrictMode first to avoid unnecessary double rendering
 const rootElement = document.getElementById("root")!;
@@ -16,6 +18,8 @@ root.render(
     <BrowserRouter>
       <ToastProvider>
         <App />
+        <Analytics />
+        <SpeedInsights />
       </ToastProvider>
     </BrowserRouter>
   </Suspense>
